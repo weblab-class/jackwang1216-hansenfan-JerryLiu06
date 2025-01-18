9e6ba9952df525a67a4c2ed86c7fe5d6727757e3
Jack Wang <jackwang@dhcp-10-29-221-217.dyn.MIT.EDU>
2025-01-18 02:16:07 -0500

    test2

--- client/src/components/modules/NavBar.jsx
+++ client/src/components/modules/NavBar.jsx
@@ -36,7 +36,7 @@ const NavBar = () => {
           <Send className="w-6 h-6" />
         </NavLink>
         <NavLink to="/howtoplay" isActive={isActive("/howtoplay")}>
-          <Send className="w-6 h-6" />
+          <CircleHelp className="w-6 h-6" />
         </NavLink>
       </div>
 
